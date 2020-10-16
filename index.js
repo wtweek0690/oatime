913d76d8d23e6d01cd5bf0b7f973d9693f60c035
Haru <68916385+haru-akiyama@users.noreply.github.com>
2020-10-16 15:42:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ client.once('ready', () => {
 client.on("message", (message)=>{
     if(message.content == "$time")
     {
-        var current_time = new Date()
+        var current_time = calcTime()
         
-        return message.channel.send(`Current time in GMT${_sign()}${config.offset.hour}:${config.offset.minutes} is ${current_time.getHours()}:${current_time.getMinutes()}`)
+        return message.channel.send(`Current time in UTC${_sign()}${config.offset.hour}:${config.offset.minutes} is ${current_time.getHours()}:${current_time.getMinutes()}`)
     }
 })
 
@@ -24,24 +24,38 @@ function _sign(){
         return "-"
 }
 
+function calcTime(){
+    var current_time = new Date()
+
+    if(config.offset.add){
+        current_time.setUTCHours(current_time.getUTCHours() + config.offset.hour)
+        current_time.setUTCMinutes(current_time.getUTCMinutes() + config.offset.minutes)
+    }else{
+        current_time.setUTCHours(current_time.getUTCHours() - config.offset.hour)
+        current_time.setUTCMinutes(current_time.getUTCMinutes() - config.offset.minutes)
+    }
+
+    return current_time
+}
+
 function time_channel_check(guildid, channelid, json, wait_time){ // string, string, array, time in millisecond 
     const guild = client.guilds.cache.get(guildid)
     const channel = guild.channels.cache.get(channelid)
     
     var channelname = channel.name;
 
-    var current_time = new Date().getTime()
+    var current_time = calcTime().getTime()
 
-    var sunrise_time = new Date()
+    var sunrise_time = calcTime()
     sunrise_time.setHours(parseInt(json[0].time.hour), parseInt(json[0].time.minute))
 
-    var morning_time = new Date()
+    var morning_time = calcTime()
     morning_time.setHours(parseInt(json[1].time.hour), parseInt(json[1].time.minute))
 
-    var sunset_time = new Date()
+    var sunset_time = calcTime()
     sunset_time.setHours(parseInt(json[2].time.hour), parseInt(json[2].time.minute))
     
-    var night_time = new Date()
+    var night_time = calcTime()
     night_time.setHours(parseInt(json[3].time.hour), parseInt(json[3].time.minute))
 
     if(night_time.getTime() - current_time < 0){
@@ -55,7 +69,6 @@ function time_channel_check(guildid, channelid, json, wait_time){ // string, str
     }
 
     if(channel.name != channelname){
-        console.log(channelname)
         channel.setName(channelname).then(()=>{
             console.log("done")
         }).catch(err => {
